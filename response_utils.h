5dacae34a7da5b5f7b22fe3f74efd7f2fbb52ad9
Juan Ignacio Elizeche Armoa <94756745+ignacioelizeche@users.noreply.github.com>
2025-06-29 11:43:23 -0700

    Update response_utils.h

--- response_utils.h
+++ response_utils.h
@@ -4,9 +4,12 @@
 #include <QString>
 #include <QHttpServerResponse>
 #include <QJsonObject>
+#include <QJsonArray>
+#include <QString>
 
 // Solo prototipos
 QHttpServerResponse createCorsResponse(const QString &content, QHttpServerResponse::StatusCode status = QHttpServerResponse::StatusCode::Ok);
 QHttpServerResponse createCorsResponse(const QJsonObject &content, QHttpServerResponse::StatusCode status = QHttpServerResponse::StatusCode::Ok);
+QHttpServerResponse createCorsResponse(const QJsonArray &content, QHttpServerResponse::StatusCode status = QHttpServerResponse::StatusCode::Ok);
 
 #endif // RESPONSE_UTILS_H
