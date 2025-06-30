4eb49a99ac6b97348a76fee03fc9185df2b24a7e
EfrainBritez <44482296+EfrainBritez@users.noreply.github.com>
2025-06-29 21:53:50 -0300

    Update ranking_routes.h
    Subiendo a mano

--- ranking_routes.h
+++ ranking_routes.h
@@ -2,33 +2,14 @@
 #define RANKING_ROUTES_H
 
 #include <QHttpServer>
-#include <QHttpServerRequest>
 #include <QHttpServerResponse>
-#include <QJsonArray>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QSqlDatabase>
-#include <QSqlError>
-#include <QSqlQuery>
-#include <QDebug>
-#include "response_utils.h"
-#include "jwt_helper.h"
+#include <QHttpServerRequest>
 
-class LeaderboardRoutes
+class RankingRoutes
 {
 public:
-    static void setupRoutes(QHttpServer *server);
-
-    // Main leaderboard endpoints
-    static QHttpServerResponse getLeaderboard(const QHttpServerRequest &request);
-    static QHttpServerResponse getWeeklyLeaderboard(const QHttpServerRequest &request);
-    static QHttpServerResponse getMonthlyLeaderboard(const QHttpServerRequest &request);
-    static QHttpServerResponse getUserRanking(const QHttpServerRequest &request, const QString &userId);
-
-    // Utility functions
-    static QJsonArray buildLeaderboardData(const QString &timeframe);
-    static QJsonArray getUserSolvedProblems(int userId);
-    static void updateLeaderboardHistory(const QString &timeframe);
+    static void setupRoutes(QHttpServer* server);
+    static QHttpServerResponse getLeaderboard(const QHttpServerRequest &req);
 };
 
-#endif // LEADERBOARD_ROUTES_H
+#endif // RANKING_ROUTES_H
